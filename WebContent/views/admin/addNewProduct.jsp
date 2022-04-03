0834fbf584880b104e34be538a04e7da13083eaa
qhuy1911 <qhuy1911@gmail.com>
2022-04-03 18:44:14 +0700

    Add file upload feature

--- WebContent/views/admin/addNewProduct.jsp
+++ WebContent/views/admin/addNewProduct.jsp
@@ -44,7 +44,7 @@
               <div class="col-12 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-                    <form class="forms-sample" action="AddProduct" method="post">
+                    <form class="forms-sample" action="AddProduct" method="post" enctype="multipart/form-data">
                       <div class="form-group">
                         <label>Name</label>
                         <input type="text" name="name" class="form-control"  placeholder="Name">
@@ -63,7 +63,7 @@
                       </div>
                       <div class="form-group">
                         <label>Image upload</label>
-                        <input type="file" name="img[]" class="file-upload-default">
+                        <input type="file" name="imageFile" class="file-upload-default">
                         <div class="input-group col-xs-12">
                           <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
                           <span class="input-group-append">
--- src/adminController/AddProductServlet.java
+++ src/adminController/AddProductServlet.java
@@ -1,13 +1,16 @@
 package adminController;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import dao.AdminDAO;
 import model.Product;
@@ -16,8 +19,14 @@ import model.Product;
  * Servlet implementation class AddProductServlet
  */
 @WebServlet("/AddProduct")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2,
+maxFileSize = 1024 * 1024 * 10,
+maxRequestSize = 1024 * 1024 * 50
+)
 public class AddProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private static final String SAVE_DIR = "images";
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -43,12 +52,24 @@ public class AddProductServlet extends HttpServlet {
 		
 		if (action.equals("Submit")) {
 			String name = request.getParameter("name");
-			String img = request.getParameter("img[]");
+			Part filePart = request.getPart("imageFile");
+			String fileName = getFileName(filePart);
 			String title = request.getParameter("title");
 			String description = request.getParameter("description");
 			double price = Double.parseDouble(request.getParameter("price"));
-			Product product = new Product(0, name, img, price, title, description);
 			
+			String appPath = "E:\\HK1_2021-2022\\LapTrinhWeb\\Project\\ShopOnline\\WebContent\\views\\user";
+			
+			String savePath = appPath + File.separator + SAVE_DIR;
+			
+			File fileSaveDir = new File(savePath);
+			if (!fileSaveDir.exists()) {
+				fileSaveDir.mkdir();
+			}
+			
+			filePart.write(savePath + File.separator + fileName);
+			
+			Product product = new Product(0, name, fileName, price, title, description);
 			AdminDAO dao = new AdminDAO();
 			boolean  success = dao.addProduct(product);
 			if (success) {
@@ -61,5 +82,16 @@ public class AddProductServlet extends HttpServlet {
 			response.sendRedirect("ProductAdmin");
 		}	
 	}
-
+	
+	private String getFileName(Part part) {
+	    final String partHeader = part.getHeader("content-disposition");
+	    for (String content : part.getHeader("content-disposition").split(";")) {
+	        if (content.trim().startsWith("filename")) {
+	            return content.substring(
+	                    content.indexOf('=') + 1).trim().replace("\"", "");
+	        }
+	    }
+	    return null;
+	}
+	
 }
